aea016c4ad40f90c09aea1e39276906c046a95ca
Dimitar Cvetanoski <dimitar.cv23@gmail.com>
2023-07-07 22:36:15 +0200

    Fully functioning custom grid creation

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,30 @@
 let container = document.querySelector('#container');
+let button = document.querySelector('#newGrid');
 
-let x = 16, y = 16;
+let size = 16;
 
-for(let i = 0; i < x*y; i++){
-    let div = document.createElement('div');
-    container.appendChild(div);
+drawGrid(size);
+
+button.addEventListener('click', () => {
+    size = Number(window.prompt('Enter a new size for the grid', `${size}`));
+    if(size>128){
+        size = 64;
+        alert(`${size} is bigger than the maximum supported size 128. Defaulting to 64`);
+    }
+    if(size){ // if a number has been inputted
+        deleteGrid(); // delete all the children from the container
+    }
+    drawGrid(size); // draw the new grid
+});
+
+function drawGrid(size){
+    for(let i = 0; i < size*size; i++){
+        let div = document.createElement('div');
+        div.style.flex = `1 0 calc(100%/${size})`
+        container.appendChild(div);
+    }
+}
+
+function deleteGrid(){
+    container.replaceChildren();
 }
